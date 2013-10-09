1b099cd5cc562791d69e07f9aa068a15a6b945c0
bluevine-dev/thelonecabbage <thelonecabbage@gmail.com>
2013-10-09 17:16:01 +0300

    head

---
+++ views.py
@@ -0,0 +1,30 @@
+import json
+from copy import copy
+
+from django.http import HttpResponse
+from django.core.urlresolvers import resolve
+
+
+def superbulk(request):
+    encoder = json.JSONEncoder()
+    data_list = json.loads(request.body)
+    res_list = []
+
+    for data in data_list:
+        view, args, kwargs = resolve(data['uri'])
+        this_request = copy(request)
+
+        #this_request._body = encoder.encode(data['body'])
+        this_request._body = data['body']
+        this_request.method = data['method']
+        kwargs['request'] = this_request
+
+        res = view(*args, **kwargs)
+        res_list.append({
+            'status_code': res.status_code,
+            'headers': res._headers,
+            'content': res.content
+        })
+
+    return HttpResponse(
+        encoder.encode(res_list), content_type='application/json')
